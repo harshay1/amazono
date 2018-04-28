14a1500bc2c8c6666ca834e92ef2dd4f15cc2d3f
Harsha Yalavarthy <harsha.hy@gmail.com>
2018-04-28 04:42:42 -0500

    Added Database connectivity to the app

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://harsha:abc123@ds161539.mlab.com:61539/proitamazono',
+    port: 3030,
+    secretKey: 'Harsha$@!#@'
+  }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan'); 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config');
 
 const app = express();
 
@@ -10,12 +11,22 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
 
-/**/
-app.listen(3030, function(err) {
+/* Database Connectivity */
+mongoose.connect(config.database, (err) => {
+    if(err)
+        {
+            console.log(err);
+        }
+    else
+        console.log('We are now connected to the database');
+});
+
+/*This is where our app is listening*/
+app.listen(config.port, function(err) {
     if(err)
         throw err;
     else
-        console.log('Magic Happens on port 3030');
+        console.log('Magic Happens on port'+ config.port);
 });
 
 /* HTTP method calls*/
