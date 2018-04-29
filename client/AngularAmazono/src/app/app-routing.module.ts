11699722df6bc1cd284e93c604d28b543a8713df
Harsha Yalavarthy <harsha.hy@gmail.com>
2018-04-28 22:30:46 -0500

    Added Profile Component client logic and included additional styles

--- client/AngularAmazono/src/app/app-routing.module.ts
+++ client/AngularAmazono/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { AuthGuardService } from './auth-guard.service';
 
 const routes: Routes = [
@@ -18,6 +19,11 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
     canActivate: [AuthGuardService]
   },
   {
